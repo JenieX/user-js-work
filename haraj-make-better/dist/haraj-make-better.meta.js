e7c3cdcb8fb54f3722333c1c80f8dd2fa4afcc16
JenieX <sozorablack@gmail.com>
2025-12-14 14:33:02 +0300

    ✨ begin the project

---
+++ haraj-make-better/dist/haraj-make-better.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name           jx-haraj-make-better
+// @version        1.0.0
+// @namespace      https://github.com/JenieX/user-js-work
+// @description    Awesome script!
+// @author         JenieX
+// @match          https://haraj.com.sa/tags/*
+// @grant          none
+// @run-at         document-start
+// @noframes
+// @compatible     chrome Violentmonkey
+// @compatible     edge Violentmonkey
+// @supportURL     https://github.com/JenieX/user-js-work/issues
+// @homepageURL    https://github.com/JenieX/user-js-work/tree/main/haraj-make-better
+// @updateURL      https://github.com/JenieX/user-js-work/raw/refs/heads/main/haraj-make-better/dist/haraj-make-better.meta.js
+// @downloadURL    https://github.com/JenieX/user-js-work/raw/refs/heads/main/haraj-make-better/dist/haraj-make-better.user.js
+// @icon           http://www.google.com/s2/favicons?domain=haraj.com.sa&sz=128
+// @license        MIT
+// ==/UserScript==
