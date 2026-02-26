41b02989e1d9b4b46c3f00cefd2bdd663310dbfc
JenieX <sozorablack@gmail.com>
2026-02-26 17:01:19 +0300

    ✨ begin the project

---
+++ haraj-no-samsung/dist/haraj-no-samsung.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name           jx-haraj-no-samsung
+// @version        1.0.0
+// @namespace      https://github.com/JenieX/user-js-work
+// @description    Awesome script!
+// @author         JenieX
+// @match          https://haraj.com.sa/*
+// @grant          none
+// @run-at         document-start
+// @noframes
+// @compatible     chrome Violentmonkey
+// @compatible     edge Violentmonkey
+// @supportURL     https://github.com/JenieX/user-js-work/issues
+// @homepageURL    https://github.com/JenieX/user-js-work/tree/main/haraj-no-samsung
+// @updateURL      https://github.com/JenieX/user-js-work/raw/refs/heads/main/haraj-no-samsung/dist/haraj-no-samsung.meta.js
+// @downloadURL    https://github.com/JenieX/user-js-work/raw/refs/heads/main/haraj-no-samsung/dist/haraj-no-samsung.user.js
+// @icon           http://www.google.com/s2/favicons?domain=haraj.com.sa&sz=128
+// @license        MIT
+// ==/UserScript==
